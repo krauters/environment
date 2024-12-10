fd127ffb463439554d2e0c81521e74a1799c6ad6
Colten Krauter <18080897+coltenkrauter@users.noreply.github.com>
2024-12-10 16:17:38 -0700

    chore: all

--- src/structures.ts
+++ src/structures.ts
@@ -1,8 +1,8 @@
-export type MapNamesToKeys<T extends readonly string[]> = { [K in T[number]]: string }
-
 export interface EnvironmentInfo<E = unknown> {
 	defaultValues: Partial<E>
 	optionalKeys: string[]
 	requiredKeys: string[]
 	transforms: Record<string, (value: string) => unknown>
 }
+
+export type MapNamesToKeys<T extends readonly string[]> = Record<T[number], string>
